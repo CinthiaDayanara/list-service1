4df346c85de241447b69b7063efc48dc9e5a0224
DayanaraChuraco <cdchuraco@uce.edu.ec>
2024-07-20 11:41:02 -0500

    update

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const app = express();
-const port = 5004;
+const port = 3003;
 
-// Conectar a MongoDB
+
 mongoose.connect('mongodb://localhost:27017/users_db', {
 
 });
 
-// Middleware para JSON
+
 app.use(express.json());
 
-// Rutas
 app.use('/', userRoutes);
 
-// Iniciar servidor
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
