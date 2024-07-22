e576870d25feb017d786011e4536fdcfe71a0c9d
SaraSaragozin <saralizeth0711@gmail.com>
2024-07-22 12:49:54 -0500

    Change to MongoAtlas

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const app = express();
 const port = 3003;
 
 
-mongoose.connect('mongodb://localhost:27017/users_db', {
+const uri = 'mongodb+srv://admin:admin@cluster0.acc1is2.mongodb.net/users_db?retryWrites=true&w=majority&appName=Cluster0';;
 
-});
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Conectado a MongoDB Atlas'))
+.catch((error) => console.error('Error al conectar a MongoDB Atlas:', error.message));
 
 app.use(cors({
   origin: '*',
